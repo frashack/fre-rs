be4e7616d3bc50b297423bbf2c449ff600abd6e4
frashack <reeceyao@qq.com>
2026-04-22 23:31:10 +0800

    chore: remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -78,11 +78,6 @@ pub mod prelude {
 /// 
 #[doc(hidden)]
 pub mod __private {
-    thread_local! {
-        /// Thread-local storage for caching panic information,
-        /// which will be propagated to the Flash Runtime.
-        pub static LAST_PANIC_INFO: std::cell::RefCell<Option<String>> = std::cell::RefCell::new(None);
-    }
     pub(crate) trait Sealed {}
 }
 
